5256564539af4ab60879748f0fd2b67a850e0845
Jordan O'Neal <0jor0238@gmail.com>
2024-06-09 19:31:20 -0400

    Fixed Screen for Mappings

--- common/src/main/java/net/londonundergroundtrains/gui/ConfigScreen.java
+++ common/src/main/java/net/londonundergroundtrains/gui/ConfigScreen.java
@@ -1,25 +1,21 @@
 package net.londonundergroundtrains.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import mtr.mappings.ScreenMapper;
 import mtr.mappings.Text;
 import mtr.screen.WidgetBetterCheckbox;
 import net.londonundergroundtrains.JSONChanger;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class ConfigScreen extends Screen {
-    private WidgetBetterCheckbox trainsComplementaryReimaginedSupport;
-
-    public ConfigScreen(Component component) {
-        super(component);
-    }
+public class ConfigScreen extends ScreenMapper {
+    private final WidgetBetterCheckbox trainsComplementaryReimaginedSupport;
 
     public ConfigScreen() {
-        this(Component.literal("Config Screen"));
+        super(Component.literal("Config Screen"));
         trainsComplementaryReimaginedSupport = new WidgetBetterCheckbox(0, 0, 0, 20, Text.translatable("gui.londonundergroundtrains.trains_complementary_reimagined_support"), checked -> {
         if (checked) {
             try {
